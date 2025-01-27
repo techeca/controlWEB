291e042a29b843b87e812d0663c7b6ee2e874d5c
techeca <jp.vasquez@outlook.com>
2025-01-26 22:20:10 -0300

    fix for sessions

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,8 +2,19 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
-  const { isAuthenticated } = useAuth();
-  return isAuthenticated ? element : <Navigate to="/signIn" />;
+  const { isAuthenticated, user, loading } = useAuth();
+  console.log(isAuthenticated);
+  console.log(user);
+
+  if (loading) {
+    return <div>Cargando...</div>; // O un spinner
+  }
+  
+  if (!isAuthenticated) {
+    return <Navigate to="/signIn" />;
+  }
+
+  return element
 };
 
 export default ProtectedRoute;
